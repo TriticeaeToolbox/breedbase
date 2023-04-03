b0cd99eb65ddfda756fa44b61fc5731b697a1660
David Waring <djw64@cornell.edu>
2023-04-03 18:04:19 -0400

    Update pull.sh
    Update the static git info after doing a pull

--- bin/utils.d/pull.sh
+++ bin/utils.d/pull.sh
@@ -29,23 +29,26 @@ else
 fi
 
 # Update each service
-echo "==> Pulling Code Upadtes..."
+echo "==> Pulling Code Updates..."
 for service in "${services[@]}"; do
     if [[ "$service" != "$DOCKER_DB_SERVICE" ]]; then
         config="$BB_CONFIG_DIR/$service.conf"
         mason_dir=$(cat "$BB_CONFIG_DIR/$service.conf" | grep "^ *add_comp_root" | awk '{$1=$1;print}' | cut -d ' ' -f 2)
 
         echo "... pulling updates into $service sgn repo"
-        cmd_sgn='cd /home/production/cxgn/sgn; git pull origin t3/master'
+        cmd_sgn='git -C /home/production/cxgn/sgn pull origin t3/master'
         "$DOCKER_COMPOSE" -f "$DOCKER_COMPOSE_FILE" exec "$service" bash -c "$cmd_sgn"
+
+        echo "... setting git version info in $service sgn repo"
+        cmd_git='/usr/local/bin/set_git_version_info'
+        "$DOCKER_COMPOSE" -f "$DOCKER_COMPOSE_FILE" exec "$service" bash -c "$cmd_git"
 
         echo "... pulling updates into $service mason repo"
         branch=$(echo "$service" | perl -pe 's/_?dev_?//g' | perl -pe 's/_/-/g' | perl -pe 's/sugarkelp/master/g')
-        cmd_mason='cd '"$mason_dir"'; git pull origin '$branch';'
+        cmd_mason='git -C '"$mason_dir"' pull origin '$branch''
         "$DOCKER_COMPOSE" -f "$DOCKER_COMPOSE_FILE" exec "$service" bash -c "$cmd_mason"
 
         echo "... reloading $service"
         "$BREEDBASE" reload "$service"
     fi
 done
-
